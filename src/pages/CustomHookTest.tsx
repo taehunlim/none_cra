b2dcc7c866e5b5e885ae20c1de4beb83366ce1a6
taehunlim <dsa1520@naver.com>
2021-07-24 15:57:39 +0900

    Add useDarkMode

--- src/pages/CustomHookTest.tsx
+++ src/pages/CustomHookTest.tsx
@@ -1,56 +1,20 @@
 import React, { useState } from 'react';
-import { useMedia } from '../Components/organisms/CustomHook/useMedia';
-import data from './data';
+import { useDarkMode } from '../Components/organisms/CustomHook/useDarkMode';
+import Toggle from './Toggle';
+import Content from './Content';
+
+import '../darkMode.scss'
 
 const CustomHookTest: React.FC = () => {
 
-  const columnCount = useMedia<number>(
-    // Media queries
-    ["(min-width: 1500px)", "(min-width: 1000px)", "(min-width: 600px)"],
-    // Column counts (relates to above media queries by array index)
-    [5, 4, 3],
-    // Default column count
-    2
-  );
-  // Create array of column heights (start at 0)
-  let columnHeights = new Array(columnCount).fill(0);
-  // Create array of arrays that will hold each column's items
-  let columns = new Array(columnCount).fill("").map(() => []) as any;
-  (data as any).forEach((item) => {
-    // Get index of shortest column
-    const shortColumnIndex = columnHeights.indexOf(Math.min(...columnHeights));
-    // Add item
-    columns[shortColumnIndex].push(item);
-    // Update height
-    columnHeights[shortColumnIndex] += item.height;
-  });
-
-  console.log(columnCount)
+  const [darkMode, setDarkMode] = useDarkMode();
 
   return (
-    <div
-      style={{
-        textAlign: "center",
-        display: "flex"
-      }}
-    >
-    
-      {columns.map((column) => (
-        <div className="column">
-          {column.map((item) => (
-            <div
-              className="image-container"
-              style={{
-                // Size image container to aspect ratio of image
-                paddingTop: (item.height / item.width) * 100 + "%",
-              }}
-            >
-              <img src={item.image} alt="" />
-            </div>
-          ))}
-        </div>
-      ))}
-
+    <div>
+      <div>
+        <Toggle darkMode={darkMode} setDarkMode={setDarkMode} />
+      </div>
+      <Content />
     </div>
   );
 };
